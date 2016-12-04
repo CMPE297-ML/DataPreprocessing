911b050d436a489d7bccc91ac6796b8b1415450f
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-12-04 14:04:17 -0800

    Fixes trial.

--- procFiles.js
+++ procFiles.js
@@ -33,35 +33,35 @@ var processFile = function(file){
   lineReader.on('line', function (line) {
     count++;
     var data = line.split(',');
-	if(data)
-	{	
-		//console.log(data);	
-		
-    var permno = data[0];
 
-    //console.log(data[1] +"=="+ file +"=="+ count );
-    if(data[1] === undefined){
-      return;
-    }
+  	if(data){	
+  		//console.log(data);
 
-    var date = new Date(data[1].substring(0,4) + "/" + data[1].substring(4,6) + "/" + data[1].substring(6,8)).getTime();
-    var price = data[46];
-    var priceInt;
+      var permno = data[0];
 
-    if(price == ''){
-      priceInt = 0;
-    }
-    else{
-      priceInt= Math.abs(parseFloat(price));
-    }
+      //console.log(data[1] +"=="+ file +"=="+ count );
+      if(data[1] === undefined){
+        return;
+      }
 
-    var queryString = "insert into stock_data (permno,date,price) values ("+permno+",'"+date+"',"+priceInt+");";
-    client.execute(queryString, function (error, result) {
-        if (!error){
-        }else{
-          console.log(error);
-        }
-    });
-	}
+      var date = new Date(data[1].substring(0,4) + "/" + data[1].substring(4,6) + "/" + data[1].substring(6,8)).getTime();
+      var price = data[46];
+      var priceInt;
+
+      if(price == ''){
+        priceInt = 0;
+      }
+      else{
+        priceInt= Math.abs(parseFloat(price));
+      }
+
+      var queryString = "insert into stock_data (permno,date,price) values ("+permno+",'"+date+"',"+priceInt+");";
+      client.execute(queryString, function (error, result) {
+          if (!error){
+          }else{
+            console.log(error);
+          }
+      });
+  	}
   });
 };
