5deffd7c903cb9606528eaf9c7403e556c7b5a79
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2016-12-04 00:07:20 -0800

    Insert data from different files..git add -A

--- index.js
+++ index.js
@@ -3,49 +3,46 @@ var fs = require('fs');
 var dbURL = '127.0.0.1';
 var client = new cassandra.Client({contactPoints: [dbURL], keyspace: 'nystock'});
 
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('./file1.csv')
-});
-var count = 0;
-lineReader.on('line', function (line) {
- var data = line.split(',');
-  var permno = data[0];
-  var date = data[1].split("");
-  var dateFormat = date[0]+date[1]+date[2]+date[3]+"/"+date[4]+date[5]+"/"+date[6]+date[7];
-  var price = data[46];
-  var priceInt;
-  if(price == ''){
-  	priceInt = 0;
-  }
-  else{
-  	priceInt= Math.abs(parseFloat(price));
-  }
-   
- 
-  var queryString = "insert into stock_data (permno,date,price) values ("+permno+",'"+dateFormat+"',"+priceInt+");";
-	client.execute(queryString, function (error, result) {
 
-      if (!error){
-      	console.log(count++);
-      }else{
-      	console.log(error);
-      }
- 	});
+//------------Read all the files in daily-files-----------------//
+const path = './daily-files/';
+fs.readdir(path, function(err, files){
+  if(err){
+    console.log("Error in reading daily-files: " + err);
+  }else{
+    console.log("Processing...!!");
+    files.forEach(function(file){
+      if(file.split(".").pop() == "csv")
+      processFile(file);
+    });
+  }
 });
 
 
-/*fs.readFile('./file1.csv', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var data = data.split(',');
-  var permno = data[0];
-  var date = data[1];
-  var price = data[46];
-  console.log(permno);
-});*/
+var processFile = function(file){
+  var lineReader = require('readline').createInterface({
+    input: fs.createReadStream(path + file)
+  });
+  lineReader.on('line', function (line) {
+    var data = line.split(',');
+    var permno = data[0];
+    var date = new Date(data[1].substring(0,4) + "/" + data[1].substring(4,6) + "/" + data[1].substring(6,8)).getTime();
+    var price = data[46];
+    var priceInt;
 
-    
+    if(price == ''){
+      priceInt = 0;
+    }
+    else{
+      priceInt= Math.abs(parseFloat(price));
+    }
 
-	console.log("outside callback");
-
+    var queryString = "insert into stock_data (permno,date,price) values ("+permno+",'"+date+"',"+priceInt+");";
+    client.execute(queryString, function (error, result) {
+        if (!error){
+        }else{
+          //console.log(error);
+        }
+    });
+  });
+};
